189ed0546016f7345ac1ed17b234cb9223257c3c
Cedric-Jefferson <46280843+Cedric-Jefferson@users.noreply.github.com>
2020-03-03 16:20:25 +0100

    Test_play_successful

---
+++ src/test/Joueur.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Joueur {
+
+	private int number;
+	private String nom;
+	
+	public Joueur(int number, String nom) {
+		super();
+		this.number = number;
+		this.nom = nom;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
+}
